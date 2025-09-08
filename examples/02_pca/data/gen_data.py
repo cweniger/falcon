14fbdada3c0ea2dee23b93aa1d8af8cddada9a4a
Christoph Weniger <c.weniger@uva.nl>
2025-09-08 12:06:39 +0200

    Fix yaml and example for 02 example

--- examples/02_pca/data/gen_data.py
+++ examples/02_pca/data/gen_data.py
@@ -1,12 +1,7 @@
 import numpy as np
 
 SIGMA = 1e-3
-NBINS = 10000
 
-# (1, NBINS) with SIGMA std, derived from standard normal distribution * SIGMA
-x = np.random.randn(1, NBINS) * SIGMA
-
-np.savez('obs_10000.npz', x=x)
-
-x = np.random.randn(NBINS) * SIGMA
-np.save('obs_10000_clean.npy', x)
+for NBINS in [100, 1000, 10000, 100000, 1000000]:
+    x = np.random.randn(NBINS) * SIGMA
+    np.save(f'obs_{NBINS}.npy', x)
