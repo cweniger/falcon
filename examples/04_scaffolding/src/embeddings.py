11fac8882883a33d0049ef94074bad68acc533ce
Christoph Weniger <c.weniger@uva.nl>
2025-12-05 09:37:22 +0100

    Current StreamingPCA state (work in progress)

--- examples/04_scaffolding/src/embeddings.py
+++ examples/04_scaffolding/src/embeddings.py
@@ -2,19 +2,6 @@ import torch
 import torch.nn
 import falcon
 
-class Linear(torch.nn.Module):
-    def __init__(self, out_features = 64):
-        super().__init__()
-        self.linear = torch.nn.LazyLinear(out_features)
-        self.norm = torch.nn.LazyBatchNorm1d()
-
-    def forward(self, x, y, n):
-        # flatten all but first dimension
-        x = x.view(x.size(0), -1)
-        x = self.norm(x)
-        x = self.linear(x)
-        return x
-
 
 class StreamingPCA(torch.nn.Module):
     """Embedding network with PCA projection and whitening.
@@ -23,18 +10,42 @@ class StreamingPCA(torch.nn.Module):
     reduction and preprocessing of high-dimensional observations.
     """
 
-    def __init__(self, log_prefix=None, num_bins=10000, num_pars=128):
+    def __init__(self, log_prefix=None, num_bins=10000, num_pars=128, record_every=100):
         super(StreamingPCA, self).__init__()
         # Import PCA components from their specific modules
         from falcon.contrib.svd import PCAProjector
         from falcon.contrib.norms import DiagonalWhitener
 
-        self.projector = PCAProjector(buffer_size=128)
+        self.projector = PCAProjector(n_components = 32, buffer_size=128)
         self.whitener = DiagonalWhitener(num_bins, use_fourier=False)
-        self.linear = torch.nn.LazyLinear(num_pars * 2)
+        #self.linear = torch.nn.LazyLinear(num_pars * 2)
         self.log_prefix = log_prefix + ":" if log_prefix else ""
         self.num_bins = num_bins
         self.num_pars = num_pars
+        self.recorder = falcon.Recorder(every = record_every, base_path = "scaffolding_pca", format='joblib')
+
+        # Simple 1D CNN for feature extraction, with batchnorm and relu activations
+#        self.cnn_1d = torch.nn.Sequential(
+#            torch.nn.Conv1d(in_channels=1, out_channels=4, kernel_size=5),
+#            torch.nn.LazyBatchNorm1d(),
+#            torch.nn.ReLU(),
+#            torch.nn.MaxPool1d(kernel_size=2),
+#            torch.nn.Conv1d(in_channels=4, out_channels=8, kernel_size=5),
+#            torch.nn.LazyBatchNorm1d(),
+#            torch.nn.ReLU(),
+#            torch.nn.MaxPool1d(kernel_size=2),
+#            torch.nn.Conv1d(in_channels=8, out_channels=16, kernel_size=5),
+#            torch.nn.LazyBatchNorm1d(),
+#            torch.nn.ReLU(),
+#            torch.nn.Flatten(),
+#        )
+        self.mlp = torch.nn.Sequential(
+            torch.nn.LazyLinear(128),
+            torch.nn.ReLU(),
+            torch.nn.LazyLinear(256),
+            torch.nn.ReLU(),
+            torch.nn.LazyLinear(32),
+        )
 
     def forward(self, x, m, n):
         falcon.log({f"{self.log_prefix}input_min": x.min().item()})
@@ -50,16 +61,31 @@ class StreamingPCA(torch.nn.Module):
         # Apply PCA projection to observation
         try:
             # falcon.log({"E:x_input_histogram": wandb.Histogram(x.detach().cpu().flatten().numpy())})
-            x = self.whitener(x)
+            x1 = self.whitener(x)
+            m1 = self.whitener(m)
             # falcon.log({"E:x_whitened_histogram": wandb.Histogram(x.detach().cpu().flatten().numpy())})
-            x = self.projector(x).float()
+            x2 = self.projector(x1).float()
             # falcon.log({"E:x_projected_histogram": wandb.Histogram(x.detach().cpu().flatten().numpy())})
+            self.recorder(lambda: dict(
+                x_input = x[:4].detach().cpu(),
+                x_whitened = x1[:4].detach().cpu(),
+                m_whitened = m1[:4].detach().cpu(),
+                x_projected = x2[:4].detach().cpu(),
+            ))
         except:
-            x = x.float()
+            x2 = x.float()
 
-        # Apply linear transformation
-        x = x / self.num_bins
-        x = self.linear(x)
+        x3 = x2 / self.num_bins**0.5/10
+        #print(1, x3.std().item())
+        # 1-dim fft, concat real and imag
+        x3 = torch.fft.fft(x3, dim=-1)
+        #print(x3.std().item())
+        x3 = torch.cat([x3.real, x3.imag], dim=-1)
+        #print(x3.std().item())
+        #x3 = self.cnn_1d(x3.unsqueeze(1))
+        x = self.mlp(x3)
+        #print(4, x.std().item())
+        #x = self.linear(x3)/1e2
         # falcon.log({"E:x_linear_compression": wandb.Histogram(x.detach().cpu().flatten().numpy())})
 
         falcon.log({f"{self.log_prefix}output_min": x.min().item()})
